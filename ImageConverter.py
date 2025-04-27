9b9f69461e6769f441267c93fc5e0b46142a7916
Jake Swanson <jakeswan213@gmail.com>
2025-04-27 10:36:46 -0500

    Image Converter file

--- ImageConverter.py
+++ ImageConverter.py
@@ -1,8 +1,21 @@
+import os
+import matplotlib.image
 import numpy as np
-import imageio
+import imageio.v2 as imageio
 
+train_direct = "ImageData/train"
 
-img = (imageio.imread(img_location, as_gray=True))/255
+allCardDirect = os.listdir(train_direct)
+card = "ImageData/train/ace of clubs/001.jpg"
+img = (imageio.imread(card, mode="F"))/255
 img = np.array(img)
+#np.savetxt("CSVData/image.csv", img, delimiter=",")
 
-np.savetxt("image.csv", a, delimiter=",")
+matplotlib.image.imsave('name.png', img)
+# for card_direct in allCardDirect:
+#     card_files = os.listdir(card_direct)
+#     for card in card_files:
+#         img = (imageio.imread(card, as_gray=True))/255
+#         img = np.array(img)
+#
+#         np.savetxt("CSVData/" + card + ".csv", img, delimiter=",")
