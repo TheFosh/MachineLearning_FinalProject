94d16d18e3acd5e86de499c3a9ea866d8b498fce
Jake Swanson <jakeswan213@gmail.com>
2025-04-29 15:58:04 -0500

    Updated csv and changed references for images read function.

--- CardCNN.py
+++ CardCNN.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 import torch
 from torch import nn
@@ -6,24 +8,27 @@ from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 from torchvision.io import read_image
 
-class CardData(Dataset):
-    def __init__(self):
-        # https://www.kaggle.com/datasets/oddrationale/mnist-in-csv
-        df = pd.read_csv("card_train.csv")
-        self.train_numbers = torch.tensor(df.iloc[:, 1:].to_numpy() / 255.0, dtype=torch.float).view(-1, 1, 224, 224)
-        self.train_labels = torch.tensor(df["label"].to_numpy())
-
-        df = pd.read_csv("card_test.csv")
-        self.test_numbers = torch.tensor(df.iloc[:, 1:].to_numpy() / 255.0, dtype=torch.float).view(-1, 1, 224, 224)
-        self.test_labels = torch.tensor(df["label"].to_numpy())
-
-        self.len = len(self.train_labels)
-
-    def __getitem__(self, item):
-        return self.train_numbers[item], self.train_labels[item]
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
 
     def __len__(self):
-        return self.len
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 1])
+        print(str(img_path))
+        image = read_image(str(img_path))
+        print(f"Read: {str(img_path)}")
+        label = self.img_labels.iloc[idx, 2]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
 
 
 class CardNetwork(nn.Module):
@@ -51,23 +56,21 @@ class CardNetwork(nn.Module):
         return self.h4_to_out(x)  # 52
 
 
-def trainNN(epochs=5, batch_size=16, lr=0.001, display_test_acc=False, trained_network=None, save_file="cardNN.pt"):
+def trainNN(epochs=5, batch_size=16, lr=0.001, display_test_acc=False):
     # load dataset
-    mnist = CardData()
+    train_cards = CustomImageDataset("ImageData\\cards.csv", "ImageData")
+    test_cards = CustomImageDataset("ImageData\\cards.csv", "ImageData")
 
     # create data loader
-    mnist_loader = DataLoader(mnist, batch_size=batch_size, drop_last=True, shuffle=True)
+    train_loader = DataLoader(train_cards, batch_size=batch_size, drop_last=True, shuffle=True)
+    test_loader = DataLoader(test_cards, batch_size=batch_size, drop_last=True, shuffle=True)
 
     # determine which device to use
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # create CNN
-    number_classify = CardNetwork().to(device)
-    if trained_network is not None:
-        number_classify.load_state_dict(trained_network)
-        number_classify.train()
-
-    print(f"Total parameters: {sum(param.numel() for param in number_classify.parameters())}")
+    card_classify = CardNetwork().to(device)
+    print(f"Total parameters: {sum(param.numel() for param in card_classify.parameters())}")
 
     # loss function
     cross_entropy = nn.CrossEntropyLoss()
@@ -78,9 +81,8 @@ def trainNN(epochs=5, batch_size=16, lr=0.001, display_test_acc=False, trained_n
     running_loss = 0.0
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1} of {epochs}")
-        for _, data in enumerate(tqdm(mnist_loader)):
+        for _, data in enumerate(tqdm(train_loader)):
             x, y = data
-
             x = x.to(device)
             y = y.to(device)
 
@@ -95,26 +97,13 @@ def trainNN(epochs=5, batch_size=16, lr=0.001, display_test_acc=False, trained_n
             optimizer.step()
 
             running_loss += loss.item()
+
         print(f"Running loss for epoch {epoch + 1}: {running_loss:.4f}")
         running_loss = 0.0
         if display_test_acc:
             with torch.no_grad():
-                predictions = torch.argmax(number_classify(mnist.test_numbers.to(device)), dim=1)  # Get the prediction
-                correct = (predictions == mnist.test_labels.to(device)).sum().item()
-                print(f"Accuracy on test set: {correct / len(mnist.test_labels):.4f}")
-
-    torch.save(number_classify.state_dict(), save_file)
-
-def predict_card(card_jpg=None, trained_model_path="cardNN.pt"):
-    if card_jpg is None:
-        print("No image.")
-        return
-
-    model = CardNetwork()
-    model.load_state_dict(torch.load(trained_model_path))
-    model.eval()
-
-    #use CNN to predict card here
+                predictions = torch.argmax(card_classify(test_loader.test_numbers.to(device)), dim=1)  # Get the prediction
+                correct = (predictions == test_loader.test_labels.to(device)).sum().item()
+                print(f"Accuracy on test set: {correct / len(test_loader.test_labels):.4f}")
 
 trainNN(epochs=5, display_test_acc=True)
-#predict_card()
