a0cbd74d51817c0c19f0b74ed75c105776e4e0fc
Jake Swanson <jakeswan213@gmail.com>
2025-05-02 11:14:27 -0500

    Worked to get NN to run on the data.

--- CardCNN.py
+++ CardCNN.py
@@ -5,10 +5,11 @@ from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import Dataset, DataLoader
 from torchvision.io import read_image
+import torchvision.transforms as transforms
 from tqdm import tqdm
 
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, is_train, transform=None, target_transform=None):
+    def __init__(self, annotations_file, img_dir, is_train, transform=transforms.Grayscale(), target_transform=None):
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
@@ -25,10 +26,10 @@ class CustomImageDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 1])
-        print(str(img_path))
-        image = read_image(str(img_path))
-        print(f"Success: {str(img_path)}")
-        label = self.img_labels.iloc[idx, 2]
+        #print(str(img_path))
+        image = read_image(str(img_path))/255
+        #print(f"Success: {str(img_path)}")
+        label = self.img_labels.iloc[idx, 0]
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
@@ -47,7 +48,7 @@ class CardNetwork(nn.Module):
         self.h2_to_h3 = nn.Conv2d(32, 8, (3, 3), padding=(1, 1))  # 8 x 112 x 112
         # Maxpool2d -> 8 x 56 x 56
         self.h3_to_h4 = nn.Linear(8 * 56 * 56, 20)  # 20
-        self.h4_to_out = nn.Linear(20, 52)  # 52
+        self.h4_to_out = nn.Linear(20, 53)  # 52
 
     def forward(self, x):
         x = F.relu(self.in_to_h1(x))  # 32 x 224 x 224
@@ -61,61 +62,6 @@ class CardNetwork(nn.Module):
         return self.h4_to_out(x)  # 52
 
 def trainNN(epochs=5, batch_size=16, lr=0.001, display_test_acc=False):
-    card_mapping = {
-        "ace of clubs": 0,
-        "ace of diamonds": 1,
-        "ace of hearts": 2,
-        "ace of spades": 3,
-        "eight of clubs": 4,
-        "eight of diamonds": 5,
-        "eight of hearts": 6,
-        "eight of spades": 7,
-        "five of clubs": 8,
-        "five of diamonds": 9,
-        "five of hearts": 10,
-        "five of spades": 11,
-        "four of clubs": 12,
-        "four of diamonds": 13,
-        "four of hearts": 14,
-        "four of spades": 15,
-        "jack of clubs": 16,
-        "jack of diamonds": 17,
-        "jack of hearts": 18,
-        "jack of spades": 19,
-        "joker": 20,
-        "king of clubs": 21,
-        "king of diamonds": 22,
-        "king of hearts": 23,
-        "king of spades": 24,
-        "nine of clubs": 25,
-        "nine of diamonds": 26,
-        "nine of hearts": 27,
-        "nine of spades": 28,
-        "queen of clubs": 29,
-        "queen of diamonds": 30,
-        "queen of hearts": 31,
-        "queen of spades": 32,
-        "seven of clubs": 33,
-        "seven of diamonds": 34,
-        "seven of hearts": 35,
-        "seven of spades": 36,
-        "six of clubs": 37,
-        "six of diamonds": 38,
-        "six of hearts": 39,
-        "six of spades": 40,
-        "ten of clubs": 41,
-        "ten of diamonds": 42,
-        "ten of hearts": 43,
-        "ten of spades": 44,
-        "three of clubs": 45,
-        "three of diamonds": 46,
-        "three of hearts": 47,
-        "three of spades": 48,
-        "two of clubs": 49,
-        "two of diamonds": 50,
-        "two of hearts": 51,
-        "two of spades": 52
-    }
 
     # load dataset
     train_cards = CustomImageDataset("ImageData\\cards.csv", "ImageData", is_train=True)
@@ -143,6 +89,7 @@ def trainNN(epochs=5, batch_size=16, lr=0.001, display_test_acc=False):
         print(f"Epoch {epoch + 1} of {epochs}")
         for _, data in enumerate(tqdm(train_loader)):
             x, y = data
+
             x = x.to(device)
             y = y.to(device)
 
@@ -162,8 +109,8 @@ def trainNN(epochs=5, batch_size=16, lr=0.001, display_test_acc=False):
         running_loss = 0.0
         if display_test_acc:
             with torch.no_grad():
-                predictions = torch.argmax(card_classify(test_loader.test_numbers.to(device)), dim=1)  # Get the prediction
-                correct = (predictions == test_loader.test_labels.to(device)).sum().item()
-                print(f"Accuracy on test set: {correct / len(test_loader.test_labels):.4f}")
+                predictions = torch.argmax(card_classify(train_cards.test_numbers.to(device)), dim=1)  # Get the prediction
+                correct = (predictions == test_cards.test_labels.to(device)).sum().item()
+                print(f"Accuracy on test set: {correct / len(test_cards.test_labels):.4f}")
 
 trainNN(epochs=5, display_test_acc=True)
